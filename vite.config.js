b4e961e24e491d72a896866a97341a0ea5a23fc8
Aditya Kumar <rkadityaav1212+git@gmail.com>
2026-07-06 19:21:31 +0530

    aa

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/FSD-Activity-React-/',
+  base: '/FSD-Activity-React/',
   server: {
     proxy: {
       '/api': 'http://localhost:3001',
